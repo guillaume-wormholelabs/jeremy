2e4e1ff8a4cf700cb2d1d931da7a7b90fb867931
Bregermann <30277759+Bregermann@users.noreply.github.com>
2021-12-07 15:07:15 -0500

    Score, Camera, ground, and skybox - Level 1!

---
+++ JeremyBregermannAngryBirdsClone/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private Text score;
+
+    private GameObject gm;
+
+    // Use this for initialization
+    private void Start()
+    {
+        score = this.gameObject.GetComponent<Text>();
+        gm = GameObject.Find("GameManager");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        score.text = "Score: " + gm.GetComponent<GameManager>().score.ToString();
+    }
+}
